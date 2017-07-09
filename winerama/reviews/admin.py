cbe0735014311e182b5395e21b7d95829b560375
michaelNinh <michaelninh@michaels-MacBook-Pro-2.local>
2017-07-09 16:12:26 -0700

    added views

--- winerama/reviews/admin.py
+++ winerama/reviews/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Review, Wine
+
+class ReviewAdmin(admin.ModelAdmin):
+    model = Review
+    list_display = ('wine', 'rating', 'user_name', 'comment', 'pub_date')
+    list_filter = ['pub_date', 'user_name']
+    search_fields = ['comment']
+
+
+admin.site.register(Wine) #cannot modify Wine from admin
+admin.site.register(Review, ReviewAdmin) #can modify Review using ReviewAdmin
