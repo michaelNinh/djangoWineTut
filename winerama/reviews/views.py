5dd845f14a70521ada62732f10cfda784f0743e6
michaelNinh <michaelninh@michaels-MacBook-Pro-2.local>
2017-07-10 10:04:08 -0700

    incompleted tut

--- winerama/reviews/views.py
+++ winerama/reviews/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Review, Wine
+from .forms import ReviewForm
+import datetime
 
 from .models import Wine, Review
 
@@ -10,7 +15,7 @@ def review_list(request):
 
 def review_detail(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
-    return render(request, 'reviews/review_detail.html', {'review': review})
+    return render(request, 'reviews/wine_detail.html', {'review': review})
 
 
 def wine_list(request):
@@ -22,3 +27,21 @@ def wine_list(request):
 def wine_detail(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
     return render(request, 'reviews/wine_detail.html', {'wine': wine})
+
+@login_required
+def add_review(request, wine_id):
+    wine = get_object_or_404(Wine, pk=wine_id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        rating = form.cleaned_data['rating']
+        comment = form.cleaned_data['comment']
+        user_name = form.cleaned_data['user_name']
+        review = Review()
+        review.wine = wine
+        review.user_name = user_name
+        review.rating = rating
+        review.comment = comment
+        review.pub_date = datetime.datetime.now()
+        review.save()
+        return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
+    return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
--- winerama/winerama/urls.py
+++ winerama/winerama/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^reviews/', include('reviews.urls', namespace="reviews")),
+url('^accounts/', include('django.contrib.auth.urls')),
 ]
 
